23a44b65ade29e17578b6698e8c0fd8ff0921bea
Leonardo Sales <lsales@globosat.com.br>
2015-10-12 12:48:45 -0300

    Aula 3 - Usando cores

--- hola_mundo.py
+++ hola_mundo.py
@@ -3,13 +3,19 @@ from pygame.locals import *
 
 __author__ = 'lsales'
 
+
+Color = (0,140,60)
+ColorDos = pygame.Color(255,120,9)
+
 pygame.init()
 
-venta = pygame.display.set_mode((400,300))
+ventana = pygame.display.set_mode((400,300))
 
 pygame.display.set_caption('Hola Mundo')
 
 while True:
+    ventana.fill(Color)
+    #ventana.fill(ColorDos)
 
     for evento in pygame.event.get():
         if evento.type == QUIT:
